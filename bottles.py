19c2086eee49ad2d3e2280770fcd3b5102e743e4
Yaroslav <nando1bone@gmail.com>
2022-10-05 18:35:51 +0300

    My commit

---
+++ bottles.py
@@ -0,0 +1,13 @@
+word = 'бутылок '
+for beer_num in range(99,0,-1):
+    print(beer_num, word, 'пива на стене.')
+    print(beer_num, word, 'пива.')
+    print('возьми одну.'); print('пусти по кругу.')
+    if beer_num == 1:
+        print('нет бутылок пива на стене.')
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = 'bottle'
+            print(new_num, word, 'of beer on the wall.')
+    print()
